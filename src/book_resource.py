505a0fa5d785e7f2fffedd4799aac7e7bc680efc
Cristian Clever <cristian@kitera.ai>
2025-12-16 16:57:01 -0300

    #versao inicial

--- src/book_resource.py
+++ src/book_resource.py
@@ -2,26 +2,31 @@ from fastapi import APIRouter, Depends
 from typing import Annotated
 from fastapi import Header, HTTPException
 import dependencies as deps
+from pydantic import BaseModel
 
+
+class Book(BaseModel):
+    id: int| None = None
+    title: str
 
 
 router = APIRouter(
     prefix="/books",
-    tags=["items"], # Optional: adds a tag for grouping in the API docs
+    tags=["books"], # Optional: adds a tag for grouping in the API docs
     responses={ 204: {"description": "Not found"}, 200: {"description":"Success"}}
     #,dependencies = [Depends(deps.get_token_header)]
 )
 
 books = list([
-    {"id": 1, "title": "Python Programming"},
-    {"id": 2, "title": "Python Programming 2"},
-    {"id": 3, "title": "Python Programming 3"},
-    {"id": 4, "title": "Python Programming 4"}
+    Book(  id=1,title= "Python Programming" ),
+Book(  id=2,title= "Java Como Programar" ),
+Book(  id=3,title= "EJB3 em ação" )
+
 ])
 
 
 @router.get("/{book_id}", tags=["books"])
-def get_book(book_id:int):
+def get_book(book_id:int ,  response_model=Book) -> Book:
     if book_id is None:
         raise HTTPException(status_code=204, detail="Book not found")
 
@@ -33,3 +38,15 @@ def get_book(book_id:int):
 
 
 
+
+@router.post("/", tags=["books"])
+def save_book(book:Book) -> Book:
+
+    if book.title is None:
+        raise HTTPException(status_code=400, detail="invalid Book title")
+
+    books.append(book)
+    book.id = len(books)+1
+
+
+    return book
